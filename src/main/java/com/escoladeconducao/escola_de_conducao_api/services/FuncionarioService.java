a464f7ee41fd59b74f0f07527dce21c5fb59dffe
Joaquim Magode <135075390+JoaquimMagode@users.noreply.github.com>
2025-11-22 05:49:49 +0530

    Refactor AlunoService to improve code readability and maintainability

--- src/main/java/com/escoladeconducao/escola_de_conducao_api/services/FuncionarioService.java
+++ src/main/java/com/escoladeconducao/escola_de_conducao_api/services/FuncionarioService.java
@@ -1,20 +1,29 @@
 package com.escoladeconducao.escola_de_conducao_api.services;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.escoladeconducao.escola_de_conducao_api.admin.Funcionario;
 import com.escoladeconducao.escola_de_conducao_api.cargos.Cargos;
+import com.escoladeconducao.escola_de_conducao_api.repository.FuncionarioRepo;
 
 @Service
 public class FuncionarioService {
 
-    
+    @Autowired
+    public final FuncionarioRepo funcionarioRepo;
+
+    public FuncionarioService(FuncionarioRepo funcionarioRepo) {
+        this.funcionarioRepo = funcionarioRepo;
+    }
 
     public List<Funcionario> getAllFuncionarios() {
-        return funcionarios;
+        return funcionarioRepo.findAll();
     }
 
     public Funcionario getFuncionarioById(int id) {
-        return funcionarios.stream()
+        return funcionarioRepo.stream()
                 .filter(p -> p.getId() == id)
                 .findFirst().orElse(new Funcionario(0, "null", Cargos.Admin, "null", "null"));
     }
