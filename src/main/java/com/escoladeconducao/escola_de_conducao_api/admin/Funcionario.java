89959da15ce3a0f3ae46ee08b352eab096168ec5
Joaquim Magode <135075390+JoaquimMagode@users.noreply.github.com>
2025-11-22 06:28:54 +0530

    Add one-to-many relationship between Funcionario and Aluno entities

--- src/main/java/com/escoladeconducao/escola_de_conducao_api/admin/Funcionario.java
+++ src/main/java/com/escoladeconducao/escola_de_conducao_api/admin/Funcionario.java
@@ -1,6 +1,10 @@
 package com.escoladeconducao.escola_de_conducao_api.admin;
 
+import java.util.List;
+
+import com.escoladeconducao.escola_de_conducao_api.aluno.Aluno;
 import com.escoladeconducao.escola_de_conducao_api.cargos.Cargos;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,6 +12,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,5 +33,9 @@ public class Funcionario {
     private Cargos cargo;
     private String email;
     private String senha;
+
+    @OneToMany(mappedBy = "funcionario")
+    @JsonManagedReference
+    private List<Aluno> alunos;
     
 }
--- src/main/java/com/escoladeconducao/escola_de_conducao_api/aluno/Aluno.java
+++ src/main/java/com/escoladeconducao/escola_de_conducao_api/aluno/Aluno.java
@@ -1,7 +1,12 @@
 package com.escoladeconducao.escola_de_conducao_api.aluno;
 
+import com.escoladeconducao.escola_de_conducao_api.admin.Funcionario;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,5 +35,8 @@ public class Aluno {
     private String senha;
     private String status;
     private String dataCadastro;
-    private int idFuncionario;
+    @ManyToOne
+    @JoinColumn(name = "id_funcionario")
+    @JsonBackReference
+    private Funcionario funcionario;
 }
